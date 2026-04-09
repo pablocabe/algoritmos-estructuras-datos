0d9a90f7c88228f08b75dcd735d07e391192c7a0
pablocabe <pablocabe0@gmail.com>
2026-04-09 16:34:38 -0300

    .

---
+++ src/tp2/ejercicio09/ParcialArboles.java
@@ -0,0 +1,13 @@
+package tp2.ejercicio09;
+
+import tp2.ejercicio01.BinaryTree;
+
+public class ParcialArboles {
+    
+    public ParcialArboles() {}
+    
+    // Wildcard (Comodín) de Genéricos
+    // public BinaryTree<?> sumAndDif(BinaryTree<Integer> arbol) {
+
+    // }
+}
