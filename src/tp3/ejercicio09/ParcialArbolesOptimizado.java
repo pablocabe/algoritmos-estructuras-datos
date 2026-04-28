b9aab2f25c482eb90d5f07f8c08749335ddd5b6f
pablocabe <pablocabe0@gmail.com>
2026-04-28 18:07:21 -0300

    .

--- src/tp3/ejercicio09/ParcialArbolesOptimizado.java
+++ src/tp3/ejercicio09/ParcialArbolesOptimizado.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 // Un árbol general es de selección si cada nodo tiene en su raíz el valor del menor de sus hijos
 
-public class ParcialArboles {
+public class ParcialArbolesOptimizado {
 
     // Devuelve true si el árbol recibido por parámetro es de selección, falso sino lo es
     public static boolean esDeSeleccion(GeneralTree<Integer> arbol) {
@@ -18,22 +18,35 @@ public class ParcialArboles {
     }
 
     private static boolean esDeSeleccionRecursivo(GeneralTree<Integer> arbol) {
-        if (arbol.isLeaf())
+        if (arbol.isLeaf()) {
             return true;
-
-        int minimoHijos = Integer.MAX_VALUE;
-        boolean ok = true;
-        List<GeneralTree<Integer>> children = arbol.getChildren();
-        Iterator<GeneralTree<Integer>> iterator = children.iterator();
-        while ((ok) && (iterator.hasNext())) {
-            GeneralTree<Integer> child = iterator.next();
-            // Math.min es como hacer if (child.getData() < minimoHijos) minimoHijos = child.getData()
-            minimoHijos = Math.min(minimoHijos, child.getData());
-            ok = esDeSeleccionRecursivo(child);
         }
 
-        return ((ok) && (arbol.getData().equals(minimoHijos)));
-    }
+        int minimoHijos = Integer.MAX_VALUE;
+        List<GeneralTree<Integer>> children = arbol.getChildren();
+        Iterator<GeneralTree<Integer>> iterator = children.iterator();
+        // Puedo usar un for en vez de un while?
+        while (iterator.hasNext()) {
+            GeneralTree<Integer> child = iterator.next();
+            if (!esDeSeleccionRecursivo(child)) {
+                return false;
+            }
+            minimoHijos = Math.min(minimoHijos, child.getData());
+        }
 
-    
+        // Si el bucle logró terminar sin que ningún "return false" lo corte, 
+        // significa que todos los hijos están bien. Solo nos queda revisar este nodo:
+        return arbol.getData().equals(minimoHijos);
+    }
 }
+
+
+/*
+// Todo el while y el iterator se resumen a esto:
+for (GeneralTree<Integer> child : arbol.getChildren()) {
+    if (!esDeSeleccionRecursivo(child)) {
+        return false;
+    }
+    minimoHijos = Math.min(minimoHijos, child.getData());
+}
+*/
---
+++ src/tp3/ejercicio10/ParcialArboles.java
@@ -0,0 +1,14 @@
+package tp3.ejercicio10;
+
+import java.util.List;
+
+import tp3.ejercicio01.GeneralTree;
+
+public class ParcialArboles {
+
+    public static List<Integer> resolver(GeneralTree<Integer> arbol) {
+
+    }
+
+    
+}
