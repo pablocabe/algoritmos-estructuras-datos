b95391721e05a6c586b6297022cd503acdc481b0
pablocabe <pablocabe0@gmail.com>
2026-04-29 11:14:53 -0300

    .

--- src/tp3/ejercicio11/ParcialArboles.java
+++ src/tp3/ejercicio11/ParcialArboles.java
@@ -8,25 +8,26 @@ import tp1.ejercicio08.Queue;
 public class ParcialArboles {
 
     public static boolean resolver(GeneralTree<Integer> arbol) {
-        if ((arbol != null) && (!arbol.isEmpty())){
+        if ((arbol != null) && (!arbol.isEmpty())) {
             boolean esCreciente = true;
             int nivelActual = 1; // Representa la cantidad de nodos que ESPERAMOS en este nivel
             Queue<GeneralTree<Integer>> cola = new Queue<>();
             cola.enqueue(arbol);
         
-            while(!cola.isEmpty() && esCreciente) {
+            while (!cola.isEmpty() && esCreciente) {
                 // 1. "Sacamos la foto": cuántos nodos hay en ESTE nivel
                 int nodosEnEsteNivel = cola.size();
                 
                 // 2. Verificamos la condición del parcial para este nivel
                 if (nivelActual != nodosEnEsteNivel) {
                     esCreciente = false;
-                } else {
+                } 
+                else {
                     // 3. Procesamos TODOS los nodos de este nivel usando la "foto"
                     for (int i = 0; i < nodosEnEsteNivel; i++) {
                         GeneralTree<Integer> aux = cola.dequeue();
                         List<GeneralTree<Integer>> children = aux.getChildren();
-                        for(GeneralTree<Integer> child: children) {
+                        for (GeneralTree<Integer> child: children) {
                             cola.enqueue(child);
                         }
                     }
