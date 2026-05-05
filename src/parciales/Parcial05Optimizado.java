99095af5a9107f78db4d8f861128b412ec937808
pablocabe <pablocabe0@gmail.com>
2026-05-04 22:56:34 -0300

    .

--- src/parciales/Parcial05Optimizado.java
+++ src/parciales/Parcial05Optimizado.java
@@ -2,19 +2,22 @@ package parciales;
 
 import tp2.ejercicio01.BinaryTree;
 
-public class Parcial05 {
+public class Parcial05Optimizado {
 
-    public int[] resolver (BinaryTree <Integer> arbol) {
-        // resultado[0] = entero par
-        // resultado[1] = nivel
-        int [] resultado = new int[2];
+    private class Resultado {
+        int numeroPar = -1;
+        int nivelActual = -1;
+    }
+
+    public Resultado resolver (BinaryTree <Integer> arbol) {
+        Resultado resultado = new Resultado();
         int nivelActual = 0;
         if ((arbol != null) && (!arbol.isEmpty()))
             recorrerArbol(arbol, resultado, nivelActual);
         return resultado;
     }
 
-    private boolean recorrerArbol (BinaryTree<Integer> nodo, int[] resultado, int nivelActual) {
+    private boolean recorrerArbol (BinaryTree<Integer> nodo, Resultado resultado, int nivelActual) {
         boolean encontrePar = false;
         if ((!encontrePar) && (nodo.hasLeftChild()))
             encontrePar = recorrerArbol(nodo.getLeftChild(), resultado, nivelActual + 1);
@@ -22,8 +25,8 @@ public class Parcial05 {
             encontrePar = recorrerArbol(nodo.getRightChild(), resultado, nivelActual + 1);
         if ((!encontrePar) && (nodo.getData() % 2 == 0)) {
             encontrePar = true;
-            resultado[0] = nodo.getData();
-            resultado[1] = nivelActual;
+            resultado.numeroPar = nodo.getData();
+            resultado.nivelActual = nivelActual;
         }
         return encontrePar;
     }
