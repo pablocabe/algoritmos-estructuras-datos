ad4a585e4d156077ca82aee169e4f2df322ddc69
pablocabe <pablocabe0@gmail.com>
2026-05-20 10:15:00 -0300

    .

--- src/tp4/ejercicio02/BuscadorEnArrayOrdenado.java
+++ src/tp4/ejercicio02/BuscadorEnArrayOrdenado.java
@@ -73,7 +73,7 @@ public class BuscadorEnArrayOrdenado {
 	}
 
 	public static void main(String[] args) {
-		int cantidadElementos = 200000;
+		int cantidadElementos = 100000;
 
 		int[] datos = initArrayOrdenado(cantidadElementos);
 
