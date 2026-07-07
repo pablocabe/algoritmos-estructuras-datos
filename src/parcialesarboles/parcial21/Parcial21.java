9c43cad1ef975b76d4fce2bb9ba97092ae0df6a6
pablocabe <pablocabe0@gmail.com>
2026-07-07 18:24:48 -0300

    .

---
+++ src/parcialesarboles/parcial21/Parcial21.java
@@ -0,0 +1,13 @@
+package parcialesarboles.parcial21;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Parcial21 {
+    
+    public List<Integer> nivel (int num) {
+        List<Integer> lista = new LinkedList<Integer>();
+
+        return lista;
+    }
+}
