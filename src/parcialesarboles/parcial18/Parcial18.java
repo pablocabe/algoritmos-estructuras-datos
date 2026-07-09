694e6a8aea9611659fc10fabcc0d13e2bb8ea25d
pablocabe <pablocabe0@gmail.com>
2026-07-09 16:39:11 -0300

    .

--- src/parcialesarboles/parcial18/Parcial18.java
+++ src/parcialesarboles/parcial18/Parcial18.java
@@ -1,5 +1,35 @@
 package parcialesarboles.parcial18;
 
+import java.util.LinkedList;
+import java.util.List;
+import tp3.ejercicio01.GeneralTree;
+
 public class Parcial18 {
-    
+
+    public List<String> resolver(int valor, GeneralTree<Integer> arbol) {
+        List<String> resultado = new LinkedList<String>();
+        if ((arbol != null) && (!arbol.isEmpty())){
+            resolverAux(arbol, valor, resultado, 1);
+        }
+        return resultado;
+    }
+
+    private void resolverAux(GeneralTree<Integer> nodoActual, int valor, List<String> resultado, int nivel) {
+        // Inorden
+        List<GeneralTree<Integer>> children = nodoActual.getChildren();
+        // Procesar hijo izquierdo
+        if (nodoActual.hasChildren()) {
+            resolverAux(children.get(0), valor, resultado, nivel + 1);
+        }
+        // Procesar actual
+        if (nodoActual.getData() > valor){
+            String elem = nodoActual.getData() + "nivel " + nivel;
+            resultado.add(elem);
+        }
+        // Procesar hijos restantes
+        for (int i = 1; i < children.size(); i++) {
+            resolverAux(children.get(i), valor, resultado, nivel + 1);
+        }
+    }
+        
 }
--- src/parcialesarboles/parcial21/Parcial21.java
+++ src/parcialesarboles/parcial21/Parcial21.java
@@ -2,12 +2,50 @@ package parcialesarboles.parcial21;
 
 import java.util.LinkedList;
 import java.util.List;
+import tp1.ejercicio08.Queue;
+import tp3.ejercicio01.GeneralTree;
 
 public class Parcial21 {
     
+    GeneralTree<Integer> arbol;
+    
     public List<Integer> nivel (int num) {
         List<Integer> lista = new LinkedList<Integer>();
-
+        if ((arbol != null) && (!arbol.isEmpty())){
+            Queue<GeneralTree<Integer>> queue = new Queue<GeneralTree<Integer>>(); 
+            queue.enqueue(this.arbol);
+            queue.enqueue(null);
+            int nivel = 0;
+            boolean encontre = false;
+            boolean nodoCumple = true;
+            while ((!queue.isEmpty()) && (!encontre)) {
+                GeneralTree<Integer> nodoActual = queue.dequeue();
+                if (nodoActual != null) {
+                    lista.add(nodoActual.getData());
+                    List<GeneralTree<Integer>> children = nodoActual.getChildren();
+                    for (GeneralTree<Integer> child: children) {
+                        queue.enqueue(child);
+                    }
+                    if (children.size() < num) {
+                        nodoCumple = false;
+                    }
+                }
+                // Entonces es null
+                else if (nodoCumple) {
+                        encontre = true;
+                }
+                // Si el nivel no cumplió siempre vaciamos la lista, sin importar si quedan más niveles
+                else {
+                    nivel++;
+                    nodoCumple = true;
+                    lista.clear();
+                    // Solo volvemos a encolar null si la cola no quedó vacía
+                    if (!queue.isEmpty())
+                        queue.enqueue(null);
+                }
+            }
+        }
         return lista;
     }
+
 }
